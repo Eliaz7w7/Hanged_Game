c74c8abb98837d18937b9827b9adabf86f203692
Elias Auque <auque.elias@gmail.com>
2022-03-18 13:20:35 -0500

    Palabra al azar

--- game.py
+++ game.py
@@ -1,14 +1,20 @@
 import os
 import random
 
-
-def run():
-    # os.system("clear")
-    print("Adivina la palabra")
+# ENUMERAMOS LA PALABRA Y LO CONVERTIMOS A DICCIONARIOS
+# DEVOLVEMOS LA VARIABLE ABC
+def dic_pal():
     with open("./DATA/data.txt", "r", encoding="utf-8") as f:
         a=[a.strip() for a in f]
         abc=dict(enumerate(a,1)) 
-    print(abc)
+    return (abc)
+
+# ESCOJEMOS UN NUMERO AL AZAR 
+# EN BASE AL NUMERO ESCOJEMOS la PALABRA DEL DICCIONARIO ANTERIOR
+def azar():
+    i=random.randint(1,len(dic_pal()))
+    rm=dic_pal()[i]
+    return rm
 
 if __name__=="__main__":
-    run()
+    azar()
